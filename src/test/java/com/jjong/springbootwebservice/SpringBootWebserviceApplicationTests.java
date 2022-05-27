1b873759b44e5487f7399d224b88515748704aad
JongkyeongPark <69239151+JongkyeongPark@users.noreply.github.com>
2022-05-27 13:40:42 +0900

    Initial commit

---
+++ src/test/java/com/jjong/springbootwebservice/SpringBootWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjong.springbootwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
